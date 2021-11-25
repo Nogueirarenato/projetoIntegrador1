837eb592d67b515806002e5db3117288824542fe
renatonogueirasenai@gmail.com <renatonogueirasenai@gmail.com>
2021-11-25 15:31:45 -0300

    up

--- FrontEnd/desperdiciozero/src/pages/CadastrarProduto.jsx
+++ FrontEnd/desperdiciozero/src/pages/CadastrarProduto.jsx
@@ -11,78 +11,123 @@ var dadosUsuarios;
 
 class CadastrarProduto extends Component {
 
-    
+
     constructor() {
-        
+
         super();
         this.state = {
-            nome:"",
-            dataDeValidade:"",
-            prazo:"",
-            valorCompra:"",
-            valorVenda:"",
+            nome: "",
+            dataDeValidade: "",
+            prazo: "",
+            valorCompra: "",
+            valorVenda: "",
             desconto: "",
-            idEstoque:"",
+            idEstoque: "",
             quantidade: "",
-            descricao:""
-            
+            descricao: ""
+
         }
     }
 
-    
+
     componentDidMount() {
         document.title = "Cadastrar Produto - Desperdicio Zero";
         this.setState({ erro: "" });
-        
+
     }
 
 
-   
 
 
-render(){
-    return (
-        <div>
-            <Header></Header>
-            <div  className= "bodyReact">
-            <h1>Cadastrar Produto</h1>
-            
-            <form onSubmit={null}>
 
+    render() {
+        return (
             <div>
-                <div>
-                    Nome do Produto
+                <Header></Header>
+                <div className="bodyReact">
+
+
+                    <div className="login5">
+                        <div className="login6">
+                            <h2>Cadastrar Produto</h2>
+                            <form onSubmit={null}>
+
+                                <div>
+                                    <div>
+                                        Nome do Produto
+                                    </div>
+                                    <input type="text" placeholder="Nome do Produto" />
+
+                                </div>
+
+                                <div>
+                                    <div>
+                                        Data de Validade
+                                    </div>
+                                    <input type="date" />
+
+                                    <div>
+                                        Anunciar faltando quantos dias para o vencimento?
+                                    </div>
+                                    <div>
+                                        <input type="text" placeholder="Digite o numero de dias..." />
+                                    </div>
+
+                                    <div>
+                                        Valor de Compra
+                                    </div>
+                                    <div>
+                                        <input type="text" placeholder="Digite o valor de compra do produto" />
+                                    </div>
+
+                                    <div>
+                                        Valor de Venda
+                                    </div>
+                                    <div>
+                                        <input type="text" placeholder="Digite o valor de venda do produto" />
+                                    </div>
+
+                                    <div>
+                                        Porcentagem de Desconto
+                                    </div>
+                                    <div>
+                                        <input type="text" placeholder="Desconto em %" />
+                                    </div>
+
+                                    <div>
+                                        Quantidade
+                                    </div>
+                                    <div>
+                                        <input type="text" placeholder="Digite a quantidade" />
+                                    </div>
+
+                                    <div>
+                                        Alguma observação?
+                                    </div>
+                                    <div>
+                                        <input type="text" placeholder="Digite a observação..." />
+                                    </div>
+
+
+
+
+                                </div>
+                            </form>
+
+
+                            <form onSubmit>
+                                <input id='1_menu' type="submit" value="Cadastrar" onClick={null} />
+                                <Link to="/">retornar a página inicial</Link>
+                            </form>
+
+
+                        </div>
+                    </div>
                 </div>
-            <input type="text" placeholder="Nome do Produto" />
-            
+                <Footer></Footer>
             </div>
-
-            <div>
-             <div>
-                 <p>Data de Validade</p>
-            </div>   
-            <input type="date" />
-
-            <div>
-                Anunciar faltando quantos dias para o vencimento?
-            </div>
-            <div>
-                <input type="text" placeholder="Digite o numero de dias..." />
-            </div>
-            
-
-
-            </div>
-            </form>
-            
-            
-
-
-            <Link to="/">retornar a página inicial</Link>
-            </div>
-            <Footer></Footer>
-        </div>
-    );
-}}
+        );
+    }
+}
 
 export default CadastrarProduto;
--- FrontEnd/desperdiciozero/src/pages/EfetuarCadastro.jsx
+++ FrontEnd/desperdiciozero/src/pages/EfetuarCadastro.jsx
@@ -11,20 +11,20 @@ import Url from '../util/apiURL';
 
 class EfetuarCadastro extends Component {
 
-    
+
     constructor() {
-        
+
         super();
         this.state = {
-            idTipo:2,
-            email:"",
-            senha:"",
-            senha01:"",
-            endereco:"",
-            documento:"",
-            telefone:"",
-            erro:"" ,
-            value:""      
+            idTipo: 2,
+            email: "",
+            senha: "",
+            senha01: "",
+            endereco: "",
+            documento: "",
+            telefone: "",
+            erro: "",
+            value: ""
         }
 
         this.atualizaIdTipo = this.atualizaIdTipo.bind(this);
@@ -34,130 +34,138 @@ class EfetuarCadastro extends Component {
         this.atualizaEndereco = this.atualizaEndereco.bind(this)
         this.atualizaDocumento = this.atualizaDocumento.bind(this)
         this.atualizaTelefone = this.atualizaTelefone.bind(this)
-        
+
     }
 
-    
+
     componentDidMount() {
         document.title = "Efetuar Cadastro - Desperdício Zero";
         this.setState({ erro: "" });
-        
-        
+
+
     }
-        atualizaIdTipo(event){
-        this.setState({idTipo: event.target.value})
+    atualizaIdTipo(event) {
+        this.setState({ idTipo: event.target.value })
         console.log(this.state.idTipo)
-        }
-       
-        atualizaEmail(event){
+    }
+
+    atualizaEmail(event) {
         console.log(this.state.idTipo)
-        this.setState({email: event.target.value})
+        this.setState({ email: event.target.value })
         console.log(this.state.email)
-        }
-        atualizaSenha(event){
-        this.setState({senha: event.target.value})
+    }
+    atualizaSenha(event) {
+        this.setState({ senha: event.target.value })
         console.log(this.state.senha)
-        }
-        atualizaSenha01(event){
-        this.setState({senha01: event.target.value})
+    }
+    atualizaSenha01(event) {
+        this.setState({ senha01: event.target.value })
         console.log(this.state.senha01)
-        }
-        atualizaEndereco(event){
-        this.setState({endereco: event.target.value})
+    }
+    atualizaEndereco(event) {
+        this.setState({ endereco: event.target.value })
         console.log(this.state.endereco)
-        }
-        atualizaDocumento(event){
-        this.setState({documento: event.target.value})
+    }
+    atualizaDocumento(event) {
+        this.setState({ documento: event.target.value })
         console.log(this.state.documento)
-        }
-        atualizaTelefone(event){
-        this.setState({telefone: event.target.value})
+    }
+    atualizaTelefone(event) {
+        this.setState({ telefone: event.target.value })
         console.log(this.state.telefone)
+    }
+
+    cadastrarUsuario(event) {
+
+        event.preventDefault();
+        if (this.state.idTipo === '2') this.setState({ idTipo: 2 })
+        if (this.state.idTipo === '3') this.setState({ idTipo: 3 })
+
+        let user = {
+            idTipo: parseInt(this.state.idTipo),
+            email: this.state.email,
+            senha: this.state.senha,
+            endereco: this.state.endereco,
+            documento: this.state.documento,
+            telefone: this.state.telefone
         }
 
-        cadastrarUsuario(event){
-         
-            event.preventDefault();
-            if(this.state.idTipo==='2')this.setState({idTipo: 2})
-            if(this.state.idTipo==='3')this.setState({idTipo: 3})
+        console.log(JSON.stringify(user))
 
-            let user = {
-                idTipo : parseInt(this.state.idTipo),
-                email : this.state.email,
-                senha : this.state.senha,
-                endereco : this.state.endereco,
-                documento : this.state.documento,
-                telefone : this.state.telefone
-            }
-
-            console.log(JSON.stringify(user))
-
-            fetch(Url + "usuarios", {
-                method: 'POST',
-                headers: {
-                    "Content-Type": "application/json",
-                },
-                body: JSON.stringify(user),
-            }
-            ).then(alert(this.state.email+" Cadastro Efetuado com sucesso, faça o login para acessar o sistema"))
+        fetch(Url + "usuarios", {
+            method: 'POST',
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(user),
+        }
+        ).then(alert(this.state.email + " Cadastro Efetuado com sucesso, faça o login para acessar o sistema"))
             .then(window.location.assign("/Login"))
 
-        }
- 
+    }
 
 
-render(){
-    return (
-        <div>
-            <Header></Header>
-            <div  className= "bodyReact">
-            <h1>Efetuar Cadastro</h1>
 
-            <form>
+    render() {
+        return (
             <div>
-            <input type="text" placeholder="email" onChange={this.atualizaEmail.bind(this)} />
-            </div>
-            <div>
-            <input type="password" placeholder="Digite uma senha" onChange={this.atualizaSenha.bind(this)}/>
-            </div>
-            <div>
-            <input type="password" placeholder="Repita a senha escolhida"onChange={this.atualizaSenha01.bind(this)}/>
-            </div>
-            <div>
-            <input type="text" placeholder= "Digite seu endereço" onChange={this.atualizaEndereco.bind(this)}/>
-            </div>
-            <div>
-            <input type="text" placeholder="Digite seu CPF ou CNPJ" onChange={this.atualizaDocumento.bind(this)}/>
-            </div>
-            <div>
-            <input type="text" placeholder= "Digite seu Telefone" onChange={this.atualizaTelefone.bind(this)}/>
-            </div>
-            <div>
-            
-            </div>
-            <form>
-            <select onChange={this.atualizaIdTipo}> 
-            <option value='2' >Vendedor</option>
-            <option value='3' >Consumidor</option>
-            
-            {this.state.value}
-            </select>
-            </form>
-            
-            </form>
-            <form onSubmit>
-                    <input id='1_menu' type="submit" value="Cadastrar" onClick={this.cadastrarUsuario.bind(this)} />
+                <Header></Header>
+                <div className="bodyReact">
 
-            </form>
+                    <div className="login3">
+                        <div className="login4">
+                            <h2>Efetuar Cadastro</h2>
 
-            
+                            <form>
+                                <div>
+                                    
+                                    <input type="text" placeholder="email" onChange={this.atualizaEmail.bind(this)} />
+                                    
+                                </div>
+                                <div>
+                                    <input type="password" placeholder="Digite uma senha" onChange={this.atualizaSenha.bind(this)} />
+                                    
+
+                                </div>
+                                <div>
+                                    <input type="password" placeholder="Repita a senha escolhida" onChange={this.atualizaSenha01.bind(this)} />
+                                </div>
+                                <div>
+                                    <input type="text" placeholder="Digite seu endereço" onChange={this.atualizaEndereco.bind(this)} />
+                                </div>
+                                <div>
+                                    <input type="text" placeholder="Digite seu CPF ou CNPJ" onChange={this.atualizaDocumento.bind(this)} />
+                                </div>
+                                <div>
+                                    <input type="text" placeholder="Digite seu Telefone" onChange={this.atualizaTelefone.bind(this)} />
+                                </div>
+                                <div>
+
+                                </div>
+                                <form>
+                                    <select onChange={this.atualizaIdTipo}>
+                                        <option value='2' >Vendedor</option>
+                                        <option value='3' >Consumidor</option>
+
+                                        {this.state.value}
+                                    </select>
+                                </form>
+
+                            </form>
+                            <form onSubmit>
+                                <input id='1_menu' type="submit" value="Cadastrar" onClick={this.cadastrarUsuario.bind(this)} />
+                                <Link to="/">retornar a página inicial</Link>
+                            </form>
+                        </div>
+                    </div>
 
 
-            <Link to="/">retornar a página inicial</Link>
+
+                </div>
+                <Footer></Footer>
             </div>
-            <Footer></Footer>
-        </div>
-    );
-}}
+        );
+    }
+}
 
 export default EfetuarCadastro;
